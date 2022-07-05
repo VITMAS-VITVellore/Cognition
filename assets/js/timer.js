ca6734773f95407c09daaf099ec55bade0746c24
nanduri_jv <vishnunanduri8@gmail.com>
2022-07-05 10:39:18 +0530

    added timer

---
+++ assets/js/timer.js
@@ -0,0 +1,19 @@
+// getting the timer nodes
+let days = document.getElementById('days');
+let hours = document.getElementById('hours');
+let minutes = document.getElementById('minutes');
+let seconds = document.getElementById('seconds');
+
+// end result
+setInterval(() =>{
+    let date1 = new Date('8/5/2022');
+    let date2 = new Date();
+    // time difference in milliseconds
+    let timeleft = Math.abs(date2 - date1);
+    // seconds remaining
+    seconds.innerHTML = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+    days.innerHTML = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    hours.innerHTML = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    minutes.innerHTML = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+},1000)

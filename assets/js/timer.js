4fe7728592be65e693bc41588f112325b31c6456
nanduri_jv <vishnunanduri8@gmail.com>
2022-07-06 15:41:58 +0530

    added particlejs

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -10,10 +10,13 @@ setInterval(() =>{
     let date2 = new Date();
     // time difference in milliseconds
     let timeleft = Math.abs(date2 - date1);
+    // days
+    days.innerHTML = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    // hours
+    hours.innerHTML = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    // minutes
+    minutes.innerHTML = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
     // seconds remaining
     seconds.innerHTML = Math.floor((timeleft % (1000 * 60)) / 1000);
-
-    days.innerHTML = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-    hours.innerHTML = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    minutes.innerHTML = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    
 },1000)
